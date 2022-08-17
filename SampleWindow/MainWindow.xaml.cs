2f97f9db5fefc89f4aa8813e900e88056a8907c2
shsx477 <shsx477@gmail.com>
2022-08-17 13:04:45 +0900

    Window 작성

---
+++ SampleWindow/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SampleWindow
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SampleWindow/MainWindowVM.cs
@@ -0,0 +1,69 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Windows;
+
+namespace SampleWindow
+{
+    public class MainWindowVM : ObservableObject
+    {
+        public static Array WindowStyleValues { get; } = Enum.GetValues(typeof(WindowStyle));
+        public static Array WindowStateValues { get; } = Enum.GetValues(typeof(WindowState));
+        public static Array ResizeModeValues { get; } = Enum.GetValues(typeof(ResizeMode));
+
+
+
+        #region MainWindowTitle
+        private string _MainWindowTitle = "Hello World";
+        public string MainWindowTitle
+        {
+            get { return _MainWindowTitle; }
+            set { SetProperty(ref _MainWindowTitle, value); }
+        }
+        #endregion
+
+        #region SelectedWindowStyle
+        private WindowStyle _SelectedWindowStyle = WindowStyle.SingleBorderWindow;
+        public WindowStyle SelectedWindowStyle
+        {
+            get { return _SelectedWindowStyle; }
+            set { SetProperty(ref _SelectedWindowStyle, value); }
+        }
+        #endregion
+
+        #region SelectedWindowState
+        private WindowState _SelectedWindowState;
+        public WindowState SelectedWindowState
+        {
+            get { return _SelectedWindowState; }
+            set { SetProperty(ref _SelectedWindowState, value); }
+        }
+        #endregion
+
+        #region SelectedResizeMode
+        private ResizeMode _SelectedResizeMode = ResizeMode.CanResize;
+        public ResizeMode SelectedResizeMode
+        {
+            get { return _SelectedResizeMode; }
+            set { SetProperty(ref _SelectedResizeMode, value); }
+        }
+        #endregion
+
+        #region Topmost
+        private bool _Topmost;
+        public bool Topmost
+        {
+            get { return _Topmost; }
+            set { SetProperty(ref _Topmost, value); }
+        }
+        #endregion
+
+        #region ShowInTaskbar
+        private bool _ShowInTaskbar = true;
+        public bool ShowInTaskbar
+        {
+            get { return _ShowInTaskbar; }
+            set { SetProperty(ref _ShowInTaskbar, value); }
+        }
+        #endregion
+    }
+}
